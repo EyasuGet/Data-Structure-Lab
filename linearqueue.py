69649816013a4192d60ae97da6dded6757b3a97f
EyasuGet <161371246+EyasuGet@users.noreply.github.com>
2024-04-19 16:59:22 +0330

    Add files via upload

---
+++ linearqueue.py
@@ -0,0 +1,32 @@
+class LinearQueue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        """Add an item to the end of the queue."""
+        self.queue.append(item)
+
+    def dequeue(self):
+        """Remove an item from the front of the queue."""
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            raise IndexError("Dequeue from an empty queue")
+
+    def is_empty(self):
+        """Check if the queue is empty."""
+        return len(self.queue) == 0
+
+    def front(self):
+        """Return the front item of the queue without removing it."""
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            raise IndexError("Front from an empty queue")
+
+queue = LinearQueue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+print(queue.dequeue())  
+print(queue.front())
